c5e5f55c8399279dbdf2fa83cd565cd237ce00c1
Ravenclaw-5 <129295263+Ravenclaw-5@users.noreply.github.com>
2025-03-05 19:52:28 -0600

    Update to client UI with leaderboard structure, just need to figure out how to get data from db.sqlite into App.js so I can put it in the leaderboard table

--- client/App.js
+++ client/App.js
@@ -1,5 +1,91 @@
 import React, { useEffect, useState } from 'react'
 
+// make the article component
+function MenuButton({value}){
+    return(
+        <article>
+            {value}
+        </article>
+    );
+}
+
+// Basic leaderboard table UI
+// How the heck am I supposed to get the db.sqlite data into this???
+function Leaderboard(){
+    return(
+        <>
+            <nav>
+                <table>
+                    <tr>
+                        <th colspan="2">Leaderboard</th>
+                    </tr>
+                    <tr>
+                        <th>Username</th>
+                        <th width="50%">Wins</th>
+                    </tr>
+                    <tr>
+                        <td>Data</td>
+                        <td>Data</td>
+                    </tr>
+                    <tr>
+                        <td>Data</td>
+                        <td>Data</td>
+                    </tr>
+                    <tr>
+                        <td>Data</td>
+                        <td>Data</td>
+                    </tr>
+                    <tr>
+                        <td>Data</td>
+                        <td>Data</td>
+                    </tr>
+                    <tr>
+                        <td>Data</td>
+                        <td>Data</td>
+                    </tr>
+                    <tr>
+                        <td>Data</td>
+                        <td>Data</td>
+                    </tr>
+                    <tr>
+                        <td>Data</td>
+                        <td>Data</td>
+                    </tr>
+                    <tr>
+                        <td>Data</td>
+                        <td>Data</td>
+                    </tr>
+                    <tr>
+                        <td>Data</td>
+                        <td>Data</td>
+                    </tr>
+                    <tr>
+                        <td>Data</td>
+                        <td>Data</td>
+                    </tr>
+                </table>
+            </nav>
+        </>
+    );
+}
+
+// make the home screen component
+function HomeScreen(){
+    return(
+        <div id = "strip">
+            <header>
+                <h1>Bit Battle 1.0.0</h1>
+            </header>
+            <br/>
+            <section>
+                <Leaderboard />
+                <MenuButton value="Start Game"/>
+                <MenuButton value="Join Game"/>
+            </section>
+        </div>
+    );
+}
+
 export default function App() {
     const [message, setMessage] = useState('');
     const [ws, setWS] = useState(null);
@@ -36,14 +122,7 @@ export default function App() {
     }, []);
 
     return (
-        // display the message from the server
-        <div id="strip">
-            <header>
-                <h1>Bit Battle 1.0.0</h1>
-            </header>
-            <article>Create Game</article>
-            <article>Join Game</article>
-            <article>Leaderboard</article>
-        </div>
+        // display the client UI
+        <HomeScreen />
     );
 }
