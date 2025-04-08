9b1a91419edfab9612cebd6957d8c8f2aa13676d
Dulworth, Luke <44180963+LukeDul@users.noreply.github.com>
2025-04-08 18:28:08 -0500

    refactored game logic into multiple files

--- server/gameplay.go
+++ server/gameplay.go
@@ -38,38 +38,6 @@ func runGameLoop(printDebug bool){
 	}
 }
 
-// initGameState initializes a gamestate struct
-func initGameState()(Gamestate){
-	gs := Gamestate {
-		player1: initPlayer(1),
-		player2: initPlayer(2),
-		targets: initTargets(),
-		projectiles: initProjectiles(),
-	}
-
-	return gs
-}
-
-// initPlayer creates a blank player
-func initPlayer(idx int)(Player){
-	return Player { x: 0, y: 0, diameter: 0, velocity: 0, idx: idx}
-}
-
-// initTargets creates the targets
-func initTargets()([]Target){
-	t1 := Target{350, 350, 10, 0, true}
-	t2 := Target{300, 300, 10, 0, true}
-	targets := []Target{t1, t2}
-	return targets
-}
-
-func initProjectiles()([]Projectile){
-	p1 := Projectile{350, 350, 10, 0, true, 0.5}
-	p2 := Projectile{300, 300, 10, 0, true, 0.5}
-	projectiles := []Projectile{p1, p2}
-	return projectiles
-}
-
 // updateGameState adjusts the gamestate based on velocities and given player input
 func updateGameState(gs Gamestate, input_queue []string)(Gamestate){
 	gs = copyGameState(gs)
--- server/gamestateStructs.go
+++ server/gamestateStructs.go
@@ -1,17 +1,4 @@
 package main
-import(
-	"time"
-)
-
-/*
-CONSTANTS THAT DEFINE ALL GAME STATES *USE THEM CAREFULLY*
-*/
-
-
-const CANVAS_WIDTH = 400;
-const CANVAS_HEIGHT = 400;
-const PLAYER_MOVE_LENGTH = 50;
-const TICK_DURATION = 1 * time.Second
 
 type Gamestate struct {
 	player1 Player;
