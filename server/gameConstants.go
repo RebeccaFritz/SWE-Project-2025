f383f17b8af3b5f375aa371c977c05b26d73bcfd
Dulworth, Luke <44180963+LukeDul@users.noreply.github.com>
2025-04-20 22:13:00 -0500

    removed semicolons

--- server/gameConstants.go
+++ server/gameConstants.go
@@ -1,13 +1,13 @@
 package main
 
-import(
+import (
 	"time"
 )
 
 /*
 CONSTANTS THAT DEFINE ALL GAME STATES *USE THEM CAREFULLY*
 */
-const CANVAS_WIDTH = 400;
-const CANVAS_HEIGHT = 400;
-const PLAYER_MOVE_LENGTH = 50;
+const CANVAS_WIDTH = 400
+const CANVAS_HEIGHT = 400
+const PLAYER_MOVE_LENGTH = 50
 const TICK_DURATION = 15 * time.Millisecond
