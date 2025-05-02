4ddac913a7d1e5f3892233f314d74df23a2407d4
Rebecca Fritz <86808098+RebeccaFritz@users.noreply.github.com>
2025-05-02 11:10:43 -0500

    Stop offscreen (#100)
    * confine player 2 to the bounds of the canvas
    
    * Confining P1 to the bounds of the canvas
    
    * reseting hex on move and changing font size to make it more readable
    
    * Changing some aesthetic and readability things

--- server/gameConstants.go
+++ server/gameConstants.go
@@ -10,5 +10,5 @@ CONSTANTS THAT DEFINE ALL GAME STATES *USE THEM CAREFULLY*
 const CANVAS_WIDTH = 400
 const CANVAS_HEIGHT = 400
 const PLAYER_MOVE_LENGTH = 50
-const TICK_DURATION = time.Second / 60
+const TICK_DURATION = time.Second / 30
 const COLLISION_ZONE = 50 // this is equal to the radius of the player token
